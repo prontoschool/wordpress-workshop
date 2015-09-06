e75f14923601b2385f4ff3977240e0648a28dab4
zkan <kan@prontomarketing.com>
2015-09-06 17:21:49 +0700

    mysql command to create db

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -7,5 +7,7 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again p
 sudo apt-get -y install mysql-server
 sudo apt-get -y install php5-fpm php5-mysql
 
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS wordpress"
+
 sudo cp /vagrant/setup/nginx/default /etc/nginx/sites-available/default
 sudo service nginx restart
